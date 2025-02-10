319884258fc253b137de28b5ad0250d5b4c6f992
Subhashree-Sharma <143106722+Subhashree-Sharma@users.noreply.github.com>
2025-02-10 10:31:48 +0530

    Delete Day_06 1 directory

--- Day_06 1/src/main/java/org/example/com/RecursiveVsIterativeFibonacci/FibonacciRecursive.java
+++
@@ -1,9 +0,0 @@
-package org.example.com.RecursiveVsIterativeFibonacci;
-public class FibonacciRecursive {
-
-    //using method for recursive fibonacci
-        public static int fibonacciRecursive(int n) {
-            if (n <= 1) return n;
-            return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
-        }
-    }
